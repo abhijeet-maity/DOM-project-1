ebe5a04813fbe0774c3f721e5620b5b0be9e5f1f
abhijeet-maity <abhijeetmaity1000@gmail.com>
2024-01-12 10:41:20 +0530

    Files added initially

---
+++ script.js
@@ -0,0 +1,8 @@
+const yellow = document.getElementById('box');
+yellow.style.backgroundColor = 'Yellow';
+yellow.style.margin = "20px";
+yellow.style.padding = "10px";
+yellow.style.fontSize = "18px";
+yellow.style.fontWeight = "bold";
+yellow.style.height = "200px";
+yellow.style.width = "300px";
